5a81af0fb9bea277a9641c7fba7363d22d4f64cc
Raquel-Morgado <raquel19012002@gmail.com>
2025-02-23 17:10:54 +0100

    version funcional y con estilos

--- blogging/src/app/app.component.ts
+++ blogging/src/app/app.component.ts
@@ -11,7 +11,21 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  entriesList: IEntry[]=[];
+  entriesList: IEntry[]= [
+    {
+      title: 'Introducción a Angular 15: Nuevas características y mejoras',
+      img: 'https://angular.io/assets/images/logos/angular/angular.svg',
+      description: 'Angular 15 ha llegado con nuevas características que facilitan el desarrollo de aplicaciones web. En este artículo, exploramos las mejoras en la nueva versión de Angular, incluyendo cambios en el rendimiento, el manejo de formularios, y el sistema de inyección de dependencias.',
+      publish_date: '22/02/2025'
+    },
+    {
+      title: 'Mejores prácticas para desarrollar una SPA con React',
+      img: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
+      description: 'En este artículo, discutimos las mejores prácticas para crear aplicaciones de una sola página (SPA) con React. Cubrimos temas como la gestión del estado, el enrutamiento, el manejo de la seguridad y la optimización de la experiencia del usuario.',
+      publish_date: '19/02/2025'
+    }
+  ];
+  ;
 
   getEntry(event: IEntry){
     this.entriesList= [...this.entriesList, event]
