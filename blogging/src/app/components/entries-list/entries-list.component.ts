59ba04aa82e2e40cb4918a4de80f722705337581
Raquel-Morgado <raquel19012002@gmail.com>
2025-02-23 14:10:07 +0100

    creamos los componentes y la interfaz necesaria

---
+++ blogging/src/app/components/entries-list/entries-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entries-list',
+  imports: [],
+  templateUrl: './entries-list.component.html',
+  styleUrl: './entries-list.component.css'
+})
+export class EntriesListComponent {
+
+}
